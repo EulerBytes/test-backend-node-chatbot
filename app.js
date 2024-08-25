6296c7a7f49abefbdfca3de49c2e2f1b986409f0
Muhammad Waleed Tahir <73441359+tahirwaleed399@users.noreply.github.com>
2024-08-25 15:55:38 +0500

    completed

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
+require('dotenv').config({ path: './Config/config.env' });
 const express = require('express');
 const app = express();
-const sampleRouter =  require('./Routes/sample.js');
-const { jsonResponce } = require('./Utils/responce.js');
+const chatRouter = require('./Routes/chat.js');
 const NewErrorHandler = require('./Utils/NewErrorHandler');
 const { ErrorController } = require('./Controllers/ErrorController.js');
+
 app.use(express.json());
-app.use(sampleRouter);
-app.all('*', (req, res , next)=>next(new NewErrorHandler('Route Not Found' , 404)))
-app.use(ErrorController)
 
+// Fix: Add a leading slash to 'api'
+app.use('/api', chatRouter);
 
-module.exports = app ;
+app.all('*', (req, res, next) => next(new NewErrorHandler('Route Not Found', 404)));
+app.use(ErrorController);
+
+module.exports = app;
